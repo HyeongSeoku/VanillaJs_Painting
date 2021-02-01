fa7dbbbe2da28fe70cd3468bac32189609a944c6
gudtjr3437 <gudtjr3437@naver.com>
2021-02-01 17:47:55 +0900

    2.3 Changing Color

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");       //선 그리기 
+const colors = document.getElementsByClassName("jsColor");
 
-const ctx = canvas.getContext("2d");        //선 그리기 
+canvas.width=700;     // 캔버스 크기 지정 (앞에서 지정한 것은 css 크기(사용자에게 보이기 위하여))
+canvas.height = 700;
 
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
-
-
 let painting = false;
-
 
 function onMouseMove(event) {
     const x = event.offsetX;
@@ -17,7 +17,7 @@ function onMouseMove(event) {
         ctx.beginPath();
         ctx.moveTo(x,y);
     } else{
-        ctx.lineTo(x,y);
+       ctx.lineTo(x,y);
         ctx.stroke();
     }
 }
@@ -38,9 +38,18 @@ function onMouseUp(event) {
     stopPainting();
 }
 
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle=color;
+}
+
+
+
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click",handleColorClick))
